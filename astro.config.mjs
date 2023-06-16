200e467f6333b483cc63710891b469f610e0969e
Tony Findeisen <tony.findeisen@onfido.com>
2023-06-17 01:07:02 +0200

    optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import relativeLinks from 'astro-relative-links';
-
 import sitemap from '@astrojs/sitemap';
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://stammbrueder.de',
-	integrations: [mdx(), sitemap(), relativeLinks()],
+  site: 'https://stammbrueder.de',
+  integrations: [mdx(), sitemap(), image({
+		serviceEntryPoint: '@astrojs/image/sharp'
+	}), relativeLinks()]
 });
